e67b34cb70bdac4c6393197f6e3c6a5f558c1680
Rob Mulla <RobMulla@users.noreply.github.com>
2024-08-08 22:59:58 -0700

    Update crucible_api.py
    Fixed bugs in crucible api and point to prod api.

--- crucible_api.py
+++ crucible_api.py
@@ -2,11 +2,12 @@ import os
 
 import requests
 from dotenv import load_dotenv
+from json.decoder import JSONDecodeError
 
 load_dotenv()
 
-BASE_URL = "https://vllm.i.staging-crucible.dreadnode.io/v1"
-API_KEY = os.getenv("CRUCIBLE_API_KEY") 
+BASE_URL = "https://crucible.dreadnode.io/api"
+API_KEY = os.getenv("CRUCIBLE_API_KEY")
 
 
 class CrucibleClient:
@@ -21,41 +22,70 @@ class CrucibleClient:
             response = requests.post(
                 f"{self.base_url}/submission", files=files, headers=self.headers
             )
+            response.raise_for_status()
+            if "submission_id" not in response.json():
+                print("Unsuccessful submission")
+                print(response.json())
+                return response.json()
             return response.json()["submission_id"]
 
     def get_submission(self, submission_id):
         url = f"{self.base_url}/submission/{submission_id}"
         response = requests.get(url, headers=self.headers)
-        return response.json()
+        try:
+            return response.json()
+        except JSONDecodeError:
+            print("Error getting submission")
+            return response
 
     def add_run_to_submission(self, submission_id, file_path):
         url = f"{self.base_url}/submission/{submission_id}/runs"
         with open(file_path, "rb") as file:
             files = {"file": (file_path, file, "application/json")}
             response = requests.put(url, files=files, headers=self.headers)
-            return response.json()
+            try:
+                return response.json()
+            except JSONDecodeError:
+                print("Error add run to submission")
+                return response
 
     def delete_run(self, submission_id, run_id):
         url = f"{self.base_url}/submission/{submission_id}/runs/{run_id}"
         response = requests.delete(url, headers=self.headers)
-        if response.status_code != 204:
-            raise Exception(response.json())
+        response.raise_for_status()
+        try:
+            return response.json()
+        except JSONDecodeError:
+            print("Error deleting run")
+            return response
 
     def upload_evidence(self, submission_id, file_path):
         url = f"{self.base_url}/submission/{submission_id}/evidence"
         with open(file_path, "rb") as file:
             files = {"file": (file_path, file, "text/plain")}
             response = requests.put(url, files=files, headers=self.headers)
+            if "evidence_id" not in response.json():
+                print("Unsuccessful upload")
+                print(response.json())
+                return response.json()
             return response.json()["evidence_id"]
 
     def delete_evidence(self, submission_id, evidence_id):
         url = f"{self.base_url}/submission/{submission_id}/evidence/{evidence_id}"
         response = requests.delete(url, headers=self.headers)
-        if response.status_code != 204:
-            raise Exception(response.json())
+        response.raise_for_status()
+        try:
+            return response.json()
+        except JSONDecodeError:
+            print("Error deleting evidence")
+            return response
 
     def delete_submission(self, submission_id):
         url = f"{self.base_url}/submission/{submission_id}"
         response = requests.delete(url, headers=self.headers)
-        if response.status_code != 204:
-            raise Exception(response.json())
+        response.raise_for_status()
+        try:
+            return response.json()
+        except JSONDecodeError:
+            print("Error deleting submission")
+            return response
